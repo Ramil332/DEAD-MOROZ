0768ef27d47836399f3e8a00d6578a4dd555c420
BigimanBaton <kirill.glushkoff2013@yandex.ru>
2023-11-11 13:22:08 +0300

    Установил новую карту, Убрал баг с лифтом

---
+++ Assets/KirillC#/Scripts/PlatformController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    [SerializeField] private Transform _uperPosition;
+    [SerializeField]private Transform _startPosition;
+
+
+    [SerializeField] private float _speedPlatform = 1f;
+
+    private Vector3 _nextPosition;
+
+    private void Start()
+    {
+        _nextPosition = transform.position;
+    }
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, _nextPosition, _speedPlatform * Time.time);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            _nextPosition = _uperPosition.position;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            _nextPosition = _startPosition.position;
+    }
+}
