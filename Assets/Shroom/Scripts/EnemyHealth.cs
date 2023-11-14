1874a033e1e6b57c648417042f691e061ca85134
ShroomikOrigin <udjolen@gmail.com>
2023-11-14 17:50:24 +0300

    VFX

--- Assets/Shroom/Scripts/EnemyHealth.cs
+++ Assets/Shroom/Scripts/EnemyHealth.cs
@@ -8,6 +8,12 @@ public class EnemyHealth : MonoBehaviour, IDamagable
     private HealthSystem _healthSystem;
 
     [SerializeField] [Range(0, 100)] private float _maxHealth;
+    [SerializeField] [Range(0, 10000)] private float _explosionForce = 1000f;
+
+    [Header("Ёффекты при смерти")]
+    [SerializeField] private Transform _vxfExplosionDie;
+    [SerializeField] private Transform _pfSnowmanParts;
+    [SerializeField] private Transform _spawnVFXPoint;
 
     private bool _died = false;
 
@@ -52,7 +58,18 @@ public class EnemyHealth : MonoBehaviour, IDamagable
         _healthSystem.OnDead -= HealthSystem_OnDead;
         _healthSystem.OnDamaged -= HealthSystem_OnDamaged;
 
-        Destroy(gameObject);
+        Instantiate(_vxfExplosionDie, _spawnVFXPoint.position, Quaternion.identity);
+        Transform SnowmanParts = Instantiate(_pfSnowmanParts, _spawnVFXPoint.position, transform.rotation);
 
+        foreach (Transform child in SnowmanParts)
+        {
+            if (child.TryGetComponent<Rigidbody>(out Rigidbody childRigidbody))
+            {
+                childRigidbody.AddExplosionForce(_explosionForce, transform.position, 5f);
+            }
+        }
+
+
+        Destroy(gameObject);
     }
 }
