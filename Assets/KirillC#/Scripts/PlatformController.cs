b2b36088581dfbeac7eb584d21ffc883a84de1fd
ShroomikOrigin <udjolen@gmail.com>
2023-11-19 13:17:10 +0300

    Lift Add

--- Assets/KirillC#/Scripts/PlatformController.cs
+++ Assets/KirillC#/Scripts/PlatformController.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class PlatformController : MonoBehaviour
 {
-    [SerializeField] private Transform _uperPosition;
-    [SerializeField]private Transform _startPosition;
-
+    [SerializeField] private Transform _upperPosition;
+    [SerializeField] private Transform _startPosition;
 
     [SerializeField] private float _speedPlatform = 1f;
 
     private Vector3 _nextPosition;
+    private bool _isGoingUp, _isGoingDown;
 
     private void Start()
     {
@@ -18,17 +18,53 @@ public class PlatformController : MonoBehaviour
     }
     private void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, _nextPosition, _speedPlatform * Time.deltaTime);
+        if (_isGoingUp)
+        {
+            MoveUp();
+        }
+
+        if (_isGoingDown)
+        {
+            transform.position = Vector3.Lerp(transform.position, _startPosition.position, _speedPlatform * Time.deltaTime);
+        }
     }
+
+    private void MoveUp()
+    {
+        if (Vector3.Distance(transform.position, _upperPosition.position) < 0.01f)
+        {
+            Debug.Log("awgfe3tg");
+            transform.position = Vector3.Lerp(transform.position, _upperPosition.position, 0 * Time.deltaTime);
+
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, _upperPosition.position, _speedPlatform * Time.deltaTime);
+        }
+
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
-            _nextPosition = _uperPosition.position;
+        if (other.CompareTag("Player"))
+        {
+            _isGoingUp = true;
+            _isGoingDown = false;
+            _nextPosition = _upperPosition.position;
+        }
+        if (other.CompareTag("LiftEndPoint"))
+        {
+            _isGoingUp = false;
+        }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.CompareTag("Player"))
+        {
+            _isGoingDown = true;
+            _isGoingUp = false;
             _nextPosition = _startPosition.position;
+        }
     }
 }
